bc4296dd5cce0ef8f13a4709e24b0b8448f39c10
Hugoldt <hugo.ladrat@epitech.digital>
2024-10-03 16:06:14 +0200

    exo5

---
+++ ex_05.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded",function(){ 
+    let addToCartBouton=document.getElementById('addToCart')
+    addToCartBouton.addEventListener('click',function(){
+    alert('thanks !')})})
